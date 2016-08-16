0a806f4b91b4fb97d20ffbb83581ced700a09e18
Will Fleming <wfleming77@gmail.com>
2016-08-15 17:47:45 -0700

    don't move active jobs to stalling set

--- lib/lua/checkStalledJobs.lua
+++ lib/lua/checkStalledJobs.lua
@@ -13,6 +13,10 @@ if a jobId is not removed from the stalling set within a stallInterval window,
 we assume the job has stalled and should be reset (moved from active back to waiting)
 --]]
 
+-- For now don't move stalling jobs back to waiting.
+-- This means we no longer have at least once delivery
+return 0
+
 local now = tonumber(ARGV[1])
 local stallTime = tonumber(redis.call("get", KEYS[1]) or 0)
 
@@ -22,10 +26,6 @@ if now < stallTime then
 end
 
 
--- For now don't move stalling jobs back to waiting.
--- This means we no longer have at least once delivery
-
---[[
 -- reset any stalling jobs by moving from active to waiting
 local stalling = redis.call("smembers", KEYS[2])
 if #stalling > 0 then
@@ -35,7 +35,7 @@ if #stalling > 0 then
   end
   redis.call("del", KEYS[2])
 end
---]]
+
 
 -- copy currently active jobs into stalling set
 local actives = redis.call("lrange", KEYS[4], 0, -1)
